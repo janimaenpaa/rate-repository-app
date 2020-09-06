d0c312b9dcc5231371e599842fbbb67bd49ec0c5
janimaenpaa <itsmayd@gmail.com>
2020-09-06 14:15:07 +0300

    Done 10.3: Add components Main, RepositoryList, RepositoryItem

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RepositoryItem = ({
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazersCount,
+  reviewCount,
+  ratingAverage,
+}) => {
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
