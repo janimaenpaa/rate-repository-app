a9754763a9052317b653b800c8df5a6cc332376e
janimaenpaa <itsmayd@gmail.com>
2020-09-07 01:25:10 +0300

    Done 10.5: Edit Main, Add styles to RepositoryItem

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,24 +1,104 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { theme } from "../theme";
 
-const RepositoryItem = ({
-  fullName,
-  description,
-  language,
-  forksCount,
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 12,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  column: {
+    flexDirection: "column",
+    alignItems: "baseline",
+  },
+  infoItem: {
+    flexDirection: "column",
+    textAlign: "center",
+  },
+  infoText: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  avatar: {
+    flexDirection: "column",
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    borderRadius: 8,
+  },
+  language: {
+    color: "white",
+    fontWeight: "bold",
+    backgroundColor: theme.languageTag,
+    padding: 8,
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 6,
+  },
+});
+
+const InfoItem = ({ value, title }) => {
+  if (value >= 1000) {
+    return (
+      <View styles={styles.infoItem}>
+        <Text style={styles.infoText}>{`${(value / 1000).toFixed(1)}k`}</Text>
+        <Text>{title}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View styles={styles.infoItem}>
+      <Text style={styles.infoText}>{value}</Text>
+      <Text>{title}</Text>
+    </View>
+  );
+};
+
+const TopBar = ({ ownerAvatarUrl, fullName, description, language }) => (
+  <View style={{ flexDirection: "row" }}>
+    <Image
+      style={styles.avatar}
+      source={{
+        uri: ownerAvatarUrl,
+      }}
+    />
+    <View style={styles.column}>
+      <Text style={{ fontWeight: "bold" }}>{fullName}</Text>
+      <Text>{description}</Text>
+      <Text style={styles.language}>{language}</Text>
+    </View>
+  </View>
+);
+
+const BottomBar = ({
   stargazersCount,
+  forksCount,
   reviewCount,
   ratingAverage,
-}) => {
+}) => (
+  <View style={styles.row}>
+    <InfoItem value={stargazersCount} title="Stars" />
+    <InfoItem value={forksCount} title="Forks" />
+    <InfoItem value={reviewCount} title="Reviews" />
+    <InfoItem value={ratingAverage} title="Rating" />
+  </View>
+);
+
+const RepositoryItem = (props) => {
   return (
-    <View>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
-      <Text>Stars: {stargazersCount}</Text>
-      <Text>Forks: {forksCount}</Text>
-      <Text>Reviews: {reviewCount}</Text>
-      <Text>Rating: {ratingAverage}</Text>
+    <View style={styles.container}>
+      <TopBar {...props} />
+      <BottomBar {...props} />
     </View>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
 export const theme = {
   appBarColor: "#24292e",
+  languageTag: "#0366d6",
+  backgroundColor: "#e1e4e8",
 };
