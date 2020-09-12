cad539147abacc36f324cb7e0ff648d3d02e68b9
janimaenpaa <itsmayd@gmail.com>
2020-09-12 18:02:32 +0300

    Done 10.14: Add authStorage.js

---
+++ src/utils/authStorage.js
@@ -0,0 +1,27 @@
+import { AsyncStorage } from "react-native";
+
+class AuthStorage {
+  constructor(namespace = "auth") {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    // Get the access token for the storage
+    await AsyncStorage.getItem(`${this.namespace}:user`);
+  }
+
+  async setAccessToken(accessToken) {
+    // Add the access token to the storage
+    await AsyncStorage.setItem(
+      `${this.namespace}:user`,
+      JSON.stringify(accessToken)
+    );
+  }
+
+  async removeAccessToken() {
+    // Remove the access token from the storage
+    await AsyncStorage.removeItem(`${this.namespace}:user`);
+  }
+}
+
+export default AuthStorage;
